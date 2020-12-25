8e740535f438a29dd3f8bdf0f572f6ff3452e071
Dalton <34592591+daltonx@users.noreply.github.com>
2020-12-25 13:14:05 -0300

    Create arrayShuffle.js

---
+++ arrayShuffle.js
@@ -0,0 +1,10 @@
+Array.prototype.shuffle = function() {
+	const size = this.length
+  	for(let i = 0; i < size; i++) {
+      const rnd = Math.floor(Math.random() * (size - 1))
+		  const tmp = this[i]
+
+		  this[i] = this[rnd]
+		  this[rnd] = tmp
+  }
+}
